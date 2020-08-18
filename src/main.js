666e0285fa08a22cee78c1f42d0971ccba518f40
zhourusheng <zhourusheng@yunzhangfang.com>
2020-08-18 12:10:56 +0800

    当命令行后不带参数时，输出帮助信息

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // 引入 commander
 import program from 'commander'
 
-// 从 package.json 获取版本号, 并解析命令行参数
+// --version 从 package.json 获取版本号, 并解析命令行参数
 const version = require('../package.json').version
-
 program.version(version).parse(process.argv)
+
+
+// --help 当命令行后不带参数时，输出帮助信息
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
