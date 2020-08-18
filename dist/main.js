b931e7de6cf1964901b3a3cfd0be024a51025cd4
zhourusheng <zhourusheng@yunzhangfang.com>
2020-08-18 11:46:35 +0800

    yfe 添加版本号

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,11 @@
 "use strict";
 
-console.log('安装babel 成功');
+var _commander = _interopRequireDefault(require("commander"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// 引入 commander
+// 从 package.json 获取版本号, 并解析命令行参数
+const version = require('../package.json').version;
+
+_commander.default.version(version).parse(process.argv);
--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+// 引入 commander
+import program from 'commander'
 
-console.log('安装babel 成功')
+// 从 package.json 获取版本号, 并解析命令行参数
+const version = require('../package.json').version
+
+program.version(version).parse(process.argv)
